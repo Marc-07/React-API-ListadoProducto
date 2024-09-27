45ffaaa0b93ab051ee1e6d9694c63cb004b547a9
Maria Ramos <maanraca01@gmail.com>
2024-09-27 08:06:09 -0600

    Se crea el componente atomico Input

---
+++ src/components/atomos/input.jsx
@@ -0,0 +1,14 @@
+const input = ({value, onChange, placeholder}) => {
+  return (
+    <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="input"
+    
+    />
+  )
+}
+
+export default input
