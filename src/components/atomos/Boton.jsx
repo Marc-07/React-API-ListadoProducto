0753fd3af6d7d93ac8b2d3172ebdf4a2e3dbe137
Maria Ramos <maanraca01@gmail.com>
2024-09-27 08:31:52 -0600

    Eliminar nombres en español de los componentes

--- src/components/atomos/Boton.jsx
+++
@@ -1,9 +0,0 @@
-const Boton = ({ onClick, label, className = "", disabled = false}) => {
-  
-    <button onClick= {onClick} className= {`btn ${className}`} disabled= {disabled}>
-        {label}
-    </button>
-  
-}
-
-export default Boton
