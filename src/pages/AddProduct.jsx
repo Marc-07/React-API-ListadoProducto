dce0520e7ed5e9a421d3d30e19737886c2330a91
Maria Ramos <maanraca01@gmail.com>
2024-09-28 13:44:09 -0600

    Añadida la funcion de HandleSubmit para buscar pokemon desde la API

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,13 +1,49 @@
+import { useState } from "react"
 import Input from "../components/atoms/input"
 import Button from "../components/atoms/Button"
 
-const AddProduct = () => {
-  return (
-    <form onSubmit={handleSubmit}>
-        <h2>Agrega un Pokemon 😼</h2>
-        <Input />
-        <Button />
-    </form>
-  )
+const AddProduct = ({onAddProduct}) => {
+
+    const [pokemonName, setPokemonName] = useState ("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+
+            if (!response.ok) throw new Error("Pokémon no encontrado");
+            const pokemonData = await response.json();
+
+            const newProduct = {
+                name: pokemonData.name,
+                image: pokemonData.sprites.other.dream_world.front_default,
+                abilities: pokemonData.abilities.map(ability => ability.ability.name).join(', '),
+            };
+
+            onAddProduct (newProduct);
+            setPokemonName("");
+        } catch (error){
+            alert ("Pokemon no encontrado, intenta de nuevo.");
+        }
+    }
+
+    
+    return (
+        
+        <form onSubmit={handleSubmit}>
+            <h2>Agrega un Pokemon 😼</h2>
+            <Input 
+                value={pokemonName}
+                onChange={(e) => setPokemonName (e.target.value) }
+                placeholder="Nombre del Pokémon"
+            />
+            <Button
+                label="Agregar"
+                disabled={!pokemonName.trim()}
+                type="submit"
+            />
+        </form>
+    )
 }
 export default AddProduct
